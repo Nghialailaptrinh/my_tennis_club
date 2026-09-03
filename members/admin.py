d31a2087e86a0de0b91c9ec85a5e4dd7e2057c85
24020251-Nguyen-Trung-Nghia <datdao420@gmail.com>
2026-09-03 14:45:09 +0700

    Thêm trường slug vào mô hình thành viên và cập nhật giao diện người dùng

--- members/admin.py
+++ members/admin.py
@@ -3,7 +3,14 @@ from .models import Member
 
 # Register your models here.
 
+
 class MemberAdmin(admin.ModelAdmin):
-  list_display = ("firstname", "lastname", "joined_date",)
+    list_display = (
+        "firstname",
+        "lastname",
+        "joined_date",
+    )
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
+
 
 admin.site.register(Member, MemberAdmin)
